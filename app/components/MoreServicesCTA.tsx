a0a55e9c10a5240c04671936a98ad317b9789d89
Ali zahid <azursoldev@gmail.com>
2025-11-26 21:56:04 +0500

    global.css update

--- app/components/MoreServicesCTA.tsx
+++ app/components/MoreServicesCTA.tsx
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faEye } from "@fortawesome/free-regular-svg-icons";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
 export default function MoreServicesCTA() {
   return (
     <section className="more-services">
@@ -7,22 +11,30 @@ export default function MoreServicesCTA() {
             More Growth <span className="accent">Services</span> from Likes.io
           </h2>
           <p className="ms-sub">
-            Instagram likes are powerful tools, but they’re not the only engagements available from Likes.io. We
+            Instagram likes are powerful tools, but they're not the only engagements available from Likes.io. We
             offer more growth services that can rapidly boost your Instagram fan base or engagement rates even more.
             Please give them a try to see how quickly you can become popular and important on the app!
           </p>
         </div>
 
         <div className="ms-actions">
-          <a href="/instagram/followers" className="ms-btn grad-violet">
-            <span className="ms-icon"><img src="/avatar.svg" alt="Followers" width={18} height={18} /></span>
+          <a href="/instagram/followers" className="ms-btn">
+            <span className="ms-icon">
+              <FontAwesomeIcon icon={faUser} />
+            </span>
             <span className="ms-label">BUY FOLLOWERS</span>
-            <span className="arrow" aria-hidden="true"></span>
+            <span className="arrow" aria-hidden="true">
+              <FontAwesomeIcon icon={faAngleRight} />
+            </span>
           </a>
-          <a href="/instagram/views" className="ms-btn grad-pink">
-            <span className="ms-icon"><img src="/eye-2.svg" alt="Views" width={18} height={18} /></span>
+          <a href="/instagram/views" className="ms-btn">
+            <span className="ms-icon">
+              <FontAwesomeIcon icon={faEye} />
+            </span>
             <span className="ms-label">BUY VIEWS</span>
-            <span className="arrow" aria-hidden="true"></span>
+            <span className="arrow" aria-hidden="true">
+              <FontAwesomeIcon icon={faAngleRight} />
+            </span>
           </a>
         </div>
       </div>
