b0ecc820427c47062f9daa994c3bb13af1019cef
Ali zahid <azursoldev@gmail.com>
2026-01-12 16:47:02 +0500

    homepage fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import PlatformSection from "./components/PlatformSection";
-import ServicesGrid from "./components/ServicesGrid";
+import InfluenceSection from "./components/InfluenceSection";
+import AdvantageSection from "./components/AdvantageSection";
+import QuickStartSection from "./components/QuickStartSection";
 import FeaturedOn from "./components/FeaturedOn";
 import ReviewsSection from "./components/ReviewsSection";
 
@@ -13,9 +15,11 @@ export default function HomePage() {
       <main>
         <Hero />
         <PlatformSection />
-        <ServicesGrid />
+        <InfluenceSection />
+        <AdvantageSection />
         <FeaturedOn />
         <ReviewsSection />
+        <QuickStartSection />
       </main>
       <Footer />
     </>
