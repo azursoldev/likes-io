189d96d3034ea7ca3efe77efce377c81a71be3d9
Shivam Saini <shivam27saini@gmail.com>
2026-04-06 20:28:32 +0100

    added header config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  poweredByHeader: false
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-developer-merchant-id-domain-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/plain',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
